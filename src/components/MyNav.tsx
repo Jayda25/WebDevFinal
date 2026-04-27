e56cd887e3fc43d11a4a3e0ff8ad687409cae069
Jayda25 <Jlopez409@mail.valenciacollege.edu>
2026-04-26 21:05:02 -0400

    'added3'

--- src/components/MyNav.tsx
+++ src/components/MyNav.tsx
@@ -3,12 +3,16 @@ import { Navbar, Container, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const MyNav: React.FC = () => {
-  
+  return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand as={Link} to="/">Recipe Blog</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">
+          Recipe Blog
+        </Navbar.Brand>
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse>
+
+        <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link as={Link} to="/">Home</Nav.Link>
             <Nav.Link as={Link} to="/recipes">Recipes</Nav.Link>
@@ -16,8 +20,10 @@ const MyNav: React.FC = () => {
             <Nav.Link as={Link} to="/about">About</Nav.Link>
           </Nav>
         </Navbar.Collapse>
+
       </Container>
     </Navbar>
+  );
 };
 
 export default MyNav;
